b1774c3f53204c31ad72e73df3a70b676d7d89dd
Prabhanshu-dev <prabhanshu4@gmail.com>
2023-10-22 14:40:57 +0530

    Dequeue  function made and praticsed

--- Pratice/src/main/java/dsa/queue/queue.java
+++ Pratice/src/main/java/dsa/queue/queue.java
@@ -16,37 +16,62 @@ public class queue {
 		}
 	}
 
-	void insertinQueue(queue d , int data) {
-		 if(d.front == null) {
-			  Queuelist newnode = new Queuelist(data);
-			  front = newnode;
-			  rear = newnode;
-			  length++;
-		 }else {
-			 Queuelist newnode = new Queuelist(data);
-			 rear.next = newnode;
-			 rear = newnode;
-			 length++;
-			 
-		 }
+	void insertinQueue(queue d, int data) {
+		if (d.front == null) {
+			Queuelist newnode = new Queuelist(data);
+			front = newnode;
+			rear = newnode;
+			length++;
+		} else {
+			Queuelist newnode = new Queuelist(data);
+			rear.next = newnode;
+			rear = newnode;
+			length++;
+
+		}
 
 	}
-	
-	void printQueue(queue object) {
-		  Queuelist temp= front;
-		  while(temp.next!=null) {
-			   System.out.print(temp.data + "-->");
-			   temp= temp.next;
-		  }
-		  System.out.print(temp.data); // last element of the queue 
-		  System.out.println();
-		  
+
+	void dequeue(queue d) {
+
+		if (d.front == null) {
+			rear = null;
+			length--;
+		} else {
+			while (d.front != null) {
+				int frontdata = d.front.data;
+				front = front.next;
+				length--;
+				System.out.println("Deleted data is " + frontdata);
+			}
+
+		}
 	}
-	
-	public static void main(String[]args) {
-		queue  object = new queue();
-		object.insertinQueue(object , 5);
+
+	void printQueue(queue object) {
+		Queuelist temp = front;
+		if (temp == null) {
+			System.out.println("Queue is empty");
+		} else {
+			while (temp.next != null) {
+				System.out.print(temp.data + "-->");
+				temp = temp.next;
+			}
+
+			System.out.print(temp.data); // last element of the queue
+			System.out.println();
+		}
+
+	}
+
+	public static void main(String[] args) {
+		queue object = new queue();
+		object.insertinQueue(object, 5);
 		object.insertinQueue(object, 10);
+		object.insertinQueue(object, 15);
+		object.insertinQueue(object, 16);
+		object.printQueue(object);
+		object.dequeue(object);
 		object.printQueue(object);
 	}
 }
